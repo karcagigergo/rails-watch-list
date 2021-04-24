8fbbf78e0bbe0def2cabef02bd7aef590d5e7089
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-24 16:09:39 -0300

    deleted movies controller, created lists controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, uniqueness: true, presence: true
+  validates :rating, inclusion: { in: (0..5),
+  message: "value has to be between 1 and 5" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :movies
+  resources :lists, only: [ :index, :show, :create ] do
+    resources :bookmarks, only: [ :new, :delete ]
+  end
 end
