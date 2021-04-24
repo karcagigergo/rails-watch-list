7163fefb296a35bbefce9e1d31679269daffbcfb
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-24 15:14:57 -0300

    Added actions to movies controller - with views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,31 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movies_params)
+    @movie.save
+    redirect_to movie_path(@movie)
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  private
+
+  def movies_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
 end
