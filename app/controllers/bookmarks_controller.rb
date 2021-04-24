bc52212647c731f0a28511ff3405a1bb51bc703b
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-24 16:44:51 -0300

    added bookmark actions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,31 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(review_params)
+    @list = List.find(params[:list_id])
+    @bookmark.restaurant = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
+  
 end
