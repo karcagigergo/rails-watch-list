e24c46bd4ab9d72c7cc955f5d4f914e27cc68876
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-25 14:26:52 -0300

    reseeded DB

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, uniqueness: true, presence: true
-  validates :rating, inclusion: { in: (0..5),
+  validates :rating, inclusion: { in: (1..10),
   message: "value has to be between 1 and 5" }
 end
