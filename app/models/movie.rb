7cd6ec00b654d83e12a94a9cdaa47a41b3aaf636
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-29 22:07:16 -0300

    i dunno whats going on

--- app/models/movie.rb
+++
@@ -1,7 +0,0 @@
-class Movie < ApplicationRecord
-  has_many :bookmarks
-  validates :title, uniqueness: true, presence: true
-  validates :overview, uniqueness: true, presence: true
-  validates :rating, inclusion: { in: (1..10),
-  message: "value has to be between 1 and 5" }
-end
