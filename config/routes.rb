1fe2ceb501c488a148725e510fd568ed5e89aba5
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-30 17:01:59 -0300

    fixed reviews model, viewn and added some style

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#home'
-  resources :lists, except: [:edit, :update] do
+  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
   resources :reviews, only: :destroy
 end
+
